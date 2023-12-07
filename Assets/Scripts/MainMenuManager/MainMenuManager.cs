680b39978fb80e1396bdb63a64fd44af8cdcce6e
Xavier Isabel <xavierisabel75@gmail.com>
2023-12-06 22:13:06 -0500

    1st commit

---
+++ Assets/Scripts/MainMenuManager/MainMenuManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private GameObject mainPanel;
+    private GameObject playPanel;
+
+    
+    void Start()
+    {
+        mainPanel = GameObject.Find("mainPanel");
+        mainPanel = GameObject.Find("playPanel");
+    }
+
+    public void ExitClicked() {
+        // Should tell streamer to quit
+        Application.Quit();
+    }
+
+    public void ExitPlayPanelClicked() {
+        playPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void BioPointPlayClicked() {
+        mainPanel.SetActive(false);
+        playPanel.SetActive(true);
+        PlayerPrefs.SetString("ControlDevice","BioPoint");
+    }
+
+    public void BioArmBandPlayClicked() {
+        mainPanel.SetActive(false);
+        playPanel.SetActive(true);
+        PlayerPrefs.SetString("ControlDevice","BioArmBand");
+        
+    }
+
+    public void KeyboardPlayClicked() {
+        PlayerPrefs.SetString("ControlMechanism","Keyboard");
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void EMGClicked() {
+        PlayerPrefs.SetString("ControlMechanism","EMG");
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void IMUClicked() {
+        PlayerPrefs.SetString("ControlMechanism","IMU");
+        SceneManager.LoadScene("MainScene");
+    }
+    void ZClicked() {
+        PlayerPrefs.SetString("ControlMechanism","Z");
+        SceneManager.LoadScene("MainScene");
+    }
+    void EMGIMUClicked() {
+        PlayerPrefs.SetString("ControlMechanism","EMGIMU");
+        SceneManager.LoadScene("MainScene");
+    }
+    void EMGZClicked() {
+        PlayerPrefs.SetString("ControlMechanism","EMGZ");
+        SceneManager.LoadScene("MainScene");
+    }
+    void IMUZClicked() {
+        PlayerPrefs.SetString("ControlMechanism","IMUZ");
+        SceneManager.LoadScene("MainScene");
+    }
+    void EMGIMUZClicked() {
+        PlayerPrefs.SetString("ControlMechanism","EMGIMUZ");
+        SceneManager.LoadScene("MainScene");
+    }
+}
